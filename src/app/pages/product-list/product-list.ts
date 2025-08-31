ffd4f96235d1a60420b8bc52ddec3030291f6f9c
Naveenbasu01 <naveennaveen69909@gmail.com>
2025-08-31 14:20:53 +0530

    firestore-setup

--- src/app/pages/product-list/product-list.ts
+++ src/app/pages/product-list/product-list.ts
@@ -1,24 +1,23 @@
-import { AfterViewInit, Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FireStore } from '../../shared/services/fire-store';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
 export default class ProductList implements OnInit {
   private fireStoreService = inject(FireStore);
-  productList: any[] = [];
+  productList$!: Observable<any[]>;
 
   ngOnInit(): void {
     this.getReq();
   }
 
   getReq() {
-    this.fireStoreService.getItems().subscribe((res: any) => {
-      // this.productList = res;
-      console.log('fire-store:', res);
-    });
+    this.productList$ = this.fireStoreService.getItems();
   }
 }
